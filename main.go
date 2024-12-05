52aa7110e7c97e3475e08b2d31090ba63b0de3a5
Luis Fellipe Batista Silva <luis.batista@irricontrol.com.br>
2024-12-05 13:41:58 -0300

    GameState e Question

--- main.go
+++ main.go
@@ -2,6 +2,18 @@ package main
 
 import "fmt"
 
+type Question struct {
+	Text string
+	Options []string
+	Answer  int
+}
+
+type GameState struct {
+	Name string
+	Points string
+	Questions []Question
+}
+
 func main() {
 	fmt.Println("Ola")
 }
