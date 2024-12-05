2fced697266488aebc4e775c3c65390fbf57c345
Luis Fellipe Batista Silva <luis.batista@irricontrol.com.br>
2024-12-05 13:55:33 -0300

    Salvando o nome do jogador

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 type Question struct {
 	Text string
@@ -14,6 +18,24 @@ type GameState struct {
 	Questions []Question
 }
 
+func (g *GameState) Init() {
+	fmt.Println("Seja bem vindo(a) ao quiz")
+	fmt.Println("Escreva o seu nome:")
+	reader := bufio.NewReader(os.Stdin)
+	
+	name, err := reader.ReadString('\n')
+
+	if err != nil {
+		panic("Erro ao ler a string")
+	}
+
+	g.Name = name
+
+	fmt.Printf("Vamos ao jogo %s", g.Name)
+
+}
+
 func main() {
-	fmt.Println("Ola")
+	game := &GameState{}
+	game.Init()
 }
