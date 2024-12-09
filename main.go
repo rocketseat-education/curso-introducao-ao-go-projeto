513fe7a0c67738f6e6c47f8a9f2ede12e6a859a8
Luis Fellipe Batista Silva <luis.batista@irricontrol.com.br>
2024-12-09 20:49:37 -0300

    Processando o arquivo csv

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 type Question struct {
@@ -16,6 +18,50 @@ type GameState struct {
 	Name string
 	Points string
 	Questions []Question
+}
+
+
+func (g *GameState) ProcessCSV() {
+	f, err := os.Open("quiz-go.csv")
+
+	if err != nil {
+		panic("erro ao ler o arquivo")
+	}
+
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+
+	records, err := reader.ReadAll()
+
+	if err != nil {
+		panic("Erro ao ler csv")
+	}
+
+	for index, record := range records {
+		fmt.Println(index, record)
+		if index > 0 {
+			question := Question {
+				Text: record[0],
+				Options: record[1:5],
+				Answer: toInt(record[5]),
+			}
+
+			g.Questions = append(g.Questions, question)
+
+		}
+	}
+
+}
+
+func toInt(s string) int {
+	i, err := strconv.Atoi(s)
+	
+	if err != nil {
+		panic(err)
+	}
+
+	return i
 }
 
 func (g *GameState) Init() {
@@ -37,5 +83,9 @@ func (g *GameState) Init() {
 
 func main() {
 	game := &GameState{}
+
+	game.ProcessCSV()
 	game.Init()
+
+	fmt.Println(game)
 }
